96ae5d3c40b7183d95b9e41cc3bbfe3c5ec0c2f1
chaduyeong <162131176+chaduyeong@users.noreply.github.com>
2025-09-02 22:51:54 +0900

    [Bronze III] Title: 곱셈, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,11 @@
+a,b,c = map(int,list(input()))
+d,e,f = map(int,list(input()))
+num1 = a*100+b*10+c
+num2 = d*100+e*10+f
+one = num1*f
+two = num1*e
+three = num1*d
+print(one)
+print(two)
+print(three)
+print(num1*num2)
