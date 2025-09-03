4aa8b7d230cafb55628f9a7cc558316deab3d13c
chaduyeong <162131176+chaduyeong@users.noreply.github.com>
2025-09-03 22:56:00 +0900

    [Bronze III] Title: 알람 시계, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.py
+++ 백준/Bronze/2884. 알람 시계/알람 시계.py
@@ -1,9 +1,10 @@
 n,m = map(int, input().split())
 if m - 45 >= 0:
-    print(n ,m-45)
+    m=m-45
 else:
-    if n == 0:
-        print(23, 60+(m-45))
-    else:
-        print(n-1,60+(m-45))
+    n=n-1
+    m=m+15
+    if n < 0:
+        n= 23
+print(n,m)
     
